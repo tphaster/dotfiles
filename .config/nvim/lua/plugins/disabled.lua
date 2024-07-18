7aca0358bb5eca5beedfa1d34c429e6c8b4373f2
Tomasz Pieczerak <tphaster@gmail.com>
2024-07-18 10:55:04 +0200

    Minor fixes in NeoVim
    * .config/nvim/lua/plugins/disabled.lua
    Disable unused plugin.
    
    * .config/nvim/lua/plugins/which-key.lua
    Remove deprecated option.

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "folke/flash.nvim", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
   { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
 }
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -4,6 +4,5 @@ return {
     plugins = {
       presets = { operators = false },
     },
-    operators = {},
   },
 }
