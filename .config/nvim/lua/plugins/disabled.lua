4dd31a7a7a15e9ab14fb963df4f352ed97d6558b
Tomasz Pieczerak <tphaster@gmail.com>
2024-10-16 13:09:19 +0200

    Disable unused NeoVim plugins, change autopairs behaviour
    * .config/nvim/lua/plugins/disabled.lua
    Disable mini.ai, nvim-snippets and friendly-snippets.
    
    * .config/nvim/lua/plugins/mini-pairs.lua
    Use autopairs only for '{}'.

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,7 @@
 return {
   { "folke/flash.nvim", enabled = false },
   { "folke/trouble.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "garymjr/nvim-snippets", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
--- .config/nvim/lua/plugins/mini-pairs.lua
+++ .config/nvim/lua/plugins/mini-pairs.lua
@@ -2,13 +2,12 @@ return {
   "echasnovski/mini.pairs",
   opts = {
     mappings = {
-      ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][^%w%p]" },
-      ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][^%w%p]" },
+      ["("] = false,
+      ["["] = false,
       ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][^%w%p]" },
-
-      ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\][^%w%p]", register = { cr = false } },
-      ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\][^%w%p]", register = { cr = false } },
-      ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\][^%w%p]", register = { cr = false } },
+      ['"'] = false,
+      ["'"] = false,
+      ["`"] = false,
     },
   },
 }
