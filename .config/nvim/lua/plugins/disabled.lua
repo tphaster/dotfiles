ec6b3b1ef00f003398bc93ff74e88dd2d4b67381
Tomasz Pieczerak <tphaster@gmail.com>
2024-10-16 13:09:19 +0200

    Disable unused NeoVim plugins
    * .config/nvim/lua/plugins/disabled.lua
    Disable mini.ai, mini.pairs, nvim-snippets and friendly-snippets.
    
    - .config/nvim/lua/plugins/mini-pairs.lua
    Remove config for disabled plugin.

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,8 @@
 return {
   { "folke/flash.nvim", enabled = false },
   { "folke/trouble.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "garymjr/nvim-snippets", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
